ea0d562e897367535d7fb68965d1bea41a0a735b
JenesesD <45162662+JenesesD@users.noreply.github.com>
2025-04-29 09:52:02 +0200

    solved fetch issues, card structure

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
-const [values, setValues] = useState([]);
+const [alkotas, setAlkotas] = useState([]);
 
 useEffect(() => {
     fetch("https://gist.githubusercontent.com/calvez/56bec47cb9c97d9999574adc65ef5368/raw/51f01cb2a160748d87dd40eae7d3785272a87355/all.json")
     .then((res) => {
-        res.json()
-    })
-    .then((data) => {
+        return res.json()
+    }).then((data) => {
         console.log(data);
-        setValues(data);
+        setAlkotas(data);
     })
-})
+}, []);
 
  return (
-    <div></div>
+    <div>
+        {alkotas.map((value) => (
+            <div key={value.alkotasAzonosito}>{value.nev}
+                <p>{value.megjelenitendoNev}</p>
+                <div>
+                    <span>{value.tipus}</span>
+                    <span>{value.keletkezesIdopontjaInt}</span>
+                </div>
+            </div>
+        ))}
+    </div>
  )
 }
 
