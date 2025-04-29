7286eefd3c29ae38613c74d5d57a1baae72d3c99
JenesesD <45162662+JenesesD@users.noreply.github.com>
2025-04-29 09:59:31 +0200

    rename

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
-const [alkotas, setAlkotas] = useState([]);
+const [alkotasok, setAlkotasok] = useState([]);
 
 useEffect(() => {
     fetch("https://gist.githubusercontent.com/calvez/56bec47cb9c97d9999574adc65ef5368/raw/51f01cb2a160748d87dd40eae7d3785272a87355/all.json")
@@ -9,18 +9,18 @@ useEffect(() => {
         return res.json()
     }).then((data) => {
         console.log(data);
-        setAlkotas(data);
+        setAlkotasok(data);
     })
 }, []);
 
  return (
     <div>
-        {alkotas.map((value) => (
-            <div key={value.alkotasAzonosito}>{value.nev}
-                <p>{value.megjelenitendoNev}</p>
+        {alkotasok.map((alkotas) => (
+            <div key={alkotas.alkotasAzonosito}>{alkotas.nev}
+                <p>{alkotas.megjelenitendoNev}</p>
                 <div>
-                    <span>{value.tipus}</span>
-                    <span>{value.keletkezesIdopontjaInt}</span>
+                    <span>{alkotas.tipus}</span>
+                    <span>{alkotas.keletkezesIdopontjaInt}</span>
                 </div>
             </div>
         ))}
