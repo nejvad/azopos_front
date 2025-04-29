4900f9b0fdc6560bcf9bd96b89db859f2a012506
JenesesD <45162662+JenesesD@users.noreply.github.com>
2025-04-29 09:33:14 +0200

    fetch

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
+const [values, setValues] = useState([]);
+
+useEffect(() => {
+    fetch("https://gist.githubusercontent.com/calvez/56bec47cb9c97d9999574adc65ef5368/raw/51f01cb2a160748d87dd40eae7d3785272a87355/all.json")
+    .then((res) => {
+        res.json()
+    })
+    .then((data) => {
+        console.log(data);
+        setValues(data);
+    })
+})
+
  return (
-    ""
+    <div></div>
  )
 }
 
